a8362c0abbf4ce62adc60a4cbf13a970406fd387
barad rajbha <135328342+rajbha9@users.noreply.github.com>
2023-08-10 18:37:04 +0530

    Add files via upload

---
+++ Class room/2.dart
@@ -0,0 +1,8 @@
+/*Write a Dart program to perform addition of two different numbers.*/
+
+void main() {
+  int a = 10, b = 20;
+  //sum of two nuumbers
+  print("sum of two numbers a and b =${a + b}");
+  print("sum of two numbers $a and $b =${a + b}");
+}
