6f68681a4bb1d92e7e67b7a2e35495f4f8492a65
barad rajbha <135328342+rajbha9@users.noreply.github.com>
2023-08-10 18:35:02 +0530

    Add files via upload

---
+++ day to day/day1.dart
@@ -0,0 +1,36 @@
+void main() {
+  int a = 10;
+  bool bool2 = true;
+  String newstrint = "devil";
+  double hj = 10.25;
+  //opretar (++,--,<,>,<=,>=,==,?,??)
+
+  var kk = 12;
+  //you dicler var he was choice autometicly which datatype use in this datatype
+  dynamic one = "hey rajbha";
+  const int a1 = 23;
+  //a1 = 66; you can not change the value of a1 becouce thar was canstant
+  final String a2;
+  //in finla you can dicler one time like a2 = "hey";
+  a2 = "hey";
+
+  ///length of String
+  String a3 = "hey rajbha barad";
+  String? a4;
+  int b = 55;
+  int c = 44;
+  print("hello"); //print hello
+  print(a);
+  print(bool2);
+  print(newstrint);
+  print(hj);
+  print(kk);
+  print(one);
+  print(a1);
+  print(a2);
+  print(a3.length);
+  print(a4?.length);
+  print(a4?.length ?? 0);
+  print("sum of value of b and c =99");
+  print("sum of value of $b and $c =${b + c}");
+}
